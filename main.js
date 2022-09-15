1b7aff796f34119489817889b6c5b8c3e9f25463
Eyad Fareh <lavna.89@gmail.com>
2022-09-15 16:09:07 +0300

    Add interactivity

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 import 'normalize.css'
 import './style.scss'
 
+document.getElementById("quote-button").addEventListener("click", updateQuote);
+
+function updateQuote(e){
+	fetch("https://api.adviceslip.com/advice").then((response) => response.json()).then((data) => {
+		console.log(data);
+		document.getElementById("advice-id").innerText = `Advice #${data.slip.id}`;
+		document.getElementById("quote").innerText = data.slip.advice;
+	})
+}
+
